e0c6694b6e102de74dd48923dde05fbe62a2433c
1iyc <liycliycliyc@gmail.com>
2019-01-31 09:52:49 +0900

    Calculate Progress

--- arrange_async_dict.py
+++ arrange_async_dict.py
@@ -18,7 +18,16 @@ def arrange_dict(dict_file, finished_file):
     dict_list = list(open(dict_file, 'r', encoding="utf-8").readlines())
     finished_list = list(open(finished_file, 'r', encoding="utf-8").readlines())
 
-    arrange_list = [line for line in dict_list if line not in finished_list]
+    dict_length = len(dict_list)
+    print("Dict List Size: {}".format(dict_length))
+    print("Finished List Size: {}".format(len(finished_list)))
+
+    arrange_list = []
+    for i, line in enumerate(dict_list):
+        if (i+1) % 10000 == 0:
+            print(str(i+1) + "/" + str(dict_length))
+        if line not in finished_list:
+            arrange_list.append(line)
 
     os.rename(dict_file, dict_file.replace(".txt", "_before.txt"))
 
