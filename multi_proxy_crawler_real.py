0c0718bf4b46486ad9ae1e012265dbd690647bf5
yiyc-kr <yiyc.kr@gmail.com>
2019-06-21 10:46:42 +0900

    feat: Develop multi_proxy_crawler_real.py

--- multi_proxy_crawler_real.py
+++ multi_proxy_crawler_real.py
@@ -2,6 +2,8 @@
 # coding: utf-8
 
 import argparse
+from datetime import datetime
+import asyncio
 import requests
 from fake_useragent import UserAgent
 from random import randint
@@ -21,36 +23,43 @@ parser.add_argument('--dict_file', dest="dict_file", type=str, default=None,
                     help="Dictionary File Path Used on Search")
 parser.add_argument('--output_file', dest="output_file", type=str, default="./data/data.txt",
                     help="Output Data File Path")
-
-parser.add_argument('--start_page', dest="start_page", type=int, default=1,
-                    help="Start Page Number")
+parser.add_argument('--finished_file', dest="finished_file", type=str, default="./data/finished_data.txt",
+                    help="Finished Data File Path")
 
 args = parser.parse_args()
 
+count = 0
 
-def crawling_items(uri, proxy_uri, word, output_file, start_page=1, last_page=0):
+
+def get_proxy(proxy_uri):
+    proxies_req = requests.get(proxy_uri, headers={'User-Agent': UserAgent().random})
+    proxies_dom = BeautifulSoup(proxies_req.content, "html.parser")
+    proxies_tr = proxies_dom.select("table > tbody > tr")
+
+    try:
+        random_int = randint(0, len(proxies_tr) - 1)
+    except Exception as e:
+        print(e)
+        return get_proxy(proxy_uri)
+
+    proxy = {
+        "http": proxies_tr[random_int].select("td")[0].text+":" + proxies_tr[random_int].select("td")[1].text,
+        "https": proxies_tr[random_int].select("td")[0].text+":" + proxies_tr[random_int].select("td")[1].text,
+    }
+    return proxy
+
+
+async def crawling_items(semaphore, uri, proxy_uri, word, total_count, output_file, finished_file,
+                         start_page=1, last_page=0):
+    await semaphore.acquire()
+    print("[ {} ]  STARTED!!!!! {}".format(word.strip(), datetime.now().strftime("%H:%M:%S %f")))
     if type(proxy_uri) == str:
-        proxies_req = requests.get(proxy_uri, headers={'User-Agent': UserAgent().random})
-        proxies_dom = BeautifulSoup(proxies_req.content, "html.parser")
-        proxies_tr = proxies_dom.select("table > tbody > tr")
-
-        proxies = []
-        for row in proxies_tr:
-            proxies.append({
-                "http": row.select("td")[0].text+":" + row.select("td")[1].text,
-                "https": row.select("td")[0].text+":" + row.select("td")[1].text,
-            })
-
-        if len(proxies) - 1:
-            proxy = proxies[randint(0, len(proxies) - 1)]
-        else:
-            crawling_items(uri, args.proxy_uri, word, output_file, start_page, last_page)
-        print("[ {} ]  PROXY IP\t{}".format(word, proxy['http']))
+        proxy = get_proxy(proxy_uri)
     else:
         proxy = proxy_uri
 
-    uri = uri.replace("???", word)
-    uri_page = uri.replace("@@@", str(start_page))
+    uri_page = uri.replace("???", word)
+    uri_page = uri_page.replace("@@@", str(start_page))
 
     try:
         req = requests.get(uri_page, proxies=proxy, timeout=5)
@@ -110,12 +119,22 @@ def crawling_items(uri, proxy_uri, word, output_file, start_page=1, last_page=0)
     sleep(uniform(0.05, 0.1))
 
 
-def main():
+async def main(loop):
     word_list = list(open(args.dict_file, 'r', encoding="utf-8").readlines())
-    for word in word_list:
-        print("[ {} ]  STARTED!!!".format(word.strip()))
-        crawling_items(args.uri, args.proxy_uri, word.strip(), args.output_file, args.start_page)
+    output_file = open(args.output_file, 'a', encoding="utf-8")
+    finished_file = open(args.finished_file, 'a', encoding="utf-8")
+
+    total_count = len(word_list)
+
+    crawler_semaphore = asyncio.Semaphore(value=10)
+    await asyncio.wait([crawling_items(crawler_semaphore, args.uri, args.proxy_uri, word.strip(),
+                                       total_count, output_file, finished_file) for word in word_list])
+
+    # print("[ {} ]  ENDED!!!!! {}".format(word.strip(), datetime.now().strftime("%H:%M:%S %f")))
 
 
 if __name__ == '__main__':
-    main()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(loop))
+    print("[!!! FINISHED !!!]")
+    loop.close()
