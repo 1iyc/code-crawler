eafa65ddbd8360d07f6484cfa6231506203f1096
yiyc-kr <yiyc.kr@gmail.com>
2019-08-07 20:07:12 +0900

    feat: Develop google_image_crawler_using_csv.py: make item directory

--- google_image_crawler_using_csv.py
+++ google_image_crawler_using_csv.py
@@ -2,23 +2,53 @@
 # coding: utf-8
 
 import argparse
-import requests
+import requests as req
 from random import uniform
 from time import sleep
 import csv
 import re
+import os
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
 
 
-def write_data(output_path, code, item, date, parse_item=False):
-    with open(output_path, 'w', encoding="utf-8") as f:
-        wr = csv.writer(f)
-        for i, v in enumerate(item):
-            if not parse_item:
-                wr.writerow([code[i], v])
-            else:
-                v_sp = v.split(';')
-                for v_item in v_sp:
-                    wr.writerow([re.sub('\D', '', code[i]), v_item.strip()])
+def crawl_google_image(keyword):
+    uri = "https://www.google.co.kr/search?"
+
+    params = {
+        "q": keyword,
+        "tbm": "isch"
+    }
+
+    html_object = req.get(uri, params)
+
+    if html_object.status_code == 200:
+        bs_object = BeautifulSoup(html_object.text, "html.parser")
+        img_data = bs_object.find_all("img")
+
+        for i in enumerate(img_data[1:]):
+            # 딕셔너리를 순서대로 넣어줌
+            t = urlopen(i[1].attrs['src']).read()
+
+            filename = "b" + str(i[0] + 1) + '.png'
+
+            with open(filename, "wb") as f:
+                f.write(t)
+
+
+def image_crawl(list_path, image_dir):
+    if not os.path.exists(image_dir):
+        os.makedirs(image_dir)
+
+    f = open(list_path, 'r', encoding='utf-8')
+    rdr = csv.reader(f)
+
+    for line in rdr:
+        code = line[0]
+        item = re.sub('[/]', '_', line[1])
+        if not os.path.exists(os.path.join(image_dir, code, item)):
+            os.makedirs(os.path.join(image_dir, code, item))
+            crawl_google_image(line[2])
 
 
 def make_search_list(csv_path, output_path, skip_column):
@@ -65,14 +95,15 @@ def main():
     parser.add_argument('--skip_column', dest="skip_column", type=bool, default=False, nargs='?', const=True,
                         help="Parse data")
 
-    parser.add_argument('--image_path', dest="image_path", type=str, default="./data/image",
+    parser.add_argument('--image_dir', dest="image_dir", type=str, default="./data/image",
                         help="Output Image Directory Path")
 
     flags, unused_flags = parser.parse_known_args()
 
-    make_search_list(flags.csv_path, flags.output_path, flags.skip_column)
+    if not os.path.isfile(flags.output_path):
+        make_search_list(flags.csv_path, flags.output_path, flags.skip_column)
 
-    write_data(flags.output_path, code, item, date, flags.parse_item)
+    image_crawl(flags.output_path, flags.image_dir)
 
 
 if __name__ == '__main__':
